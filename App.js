cc3dbf25afebdedc57bb39f86507dc084ad03df8
Tusharborate <borate.tushar@gmail.com>
2023-10-25 12:13:23 +0530

    episode-01

---
+++ App.js
@@ -0,0 +1,20 @@
+const heading = React.createElement("div",{},
+                [
+                React.createElement("div",{id: "heading"},
+                    [
+                        React.createElement("h1", {id: "inner"}, "This is heading one"),
+                        React.createElement("h2", {id: "inner2"}, "This is heading text two")
+                    ]
+                ),
+                React.createElement("div",{id: "heading"},
+                    [
+                        React.createElement("h1", {id: "inner"}, "This is heading one"),
+                        React.createElement("h2", {id: "inner2"}, "This is heading text two")
+                    ]
+                )
+                ]
+                );
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(heading);
