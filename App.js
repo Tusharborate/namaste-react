1d8b5824570fd90caed0e11ae98f83906a1e3045
Tusharborate <borate.tushar@gmail.com>
2023-10-27 13:25:37 +0530

    Functional Commponent

--- App.js
+++ App.js
@@ -1,42 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-/*
-const heading = React.createElement("div",{},
-                [
-                React.createElement("div",{id: "heading"},
-                    [
-                        React.createElement("h1", {id: "inner"}, "This is heading one"),
-                        React.createElement("h2", {id: "inner2"}, "This is heading text two")
-                    ]
-                ),
-                React.createElement("div",{id: "heading"},
-                    [
-                        React.createElement("h1", {id: "inner"}, "This is heading one"),
-                        React.createElement("h2", {id: "inner2"}, "This is heading text two")
-                    ]
-                )
-                ]
-                );
-*/
 
-const heading = React.createElement("h1",{id: "heading"},"This is header");
-console.log(heading);
-
-const jsxElement = <h1 id="heading" tabIndex={1}>This is header</h1>; 
-console.log(jsxElement);
-
-const TitleComponent = () => (
-    <h1 id="heading">This is title</h1>
+const Elem = () => (
+    <h2>This is testing heading {22 + 32}</h2>
 )
 
-const HeadingComponent = () => (
-    <div className='container'>
-        <TitleComponent/>
-    </div>
-)
-
-
+const elem = (
+    <>
+        <h3>This is heading {22 + 54}</h3>
+        {
+            console.log("Working")
+        }
+        <Elem/>
+    </>
+) 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-// This is comment
-root.render(<HeadingComponent/>);
+
+root.render(elem); 
