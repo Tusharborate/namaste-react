01e2d1c13a67d7599b737339e92707ffdf1e7fe4
Tusharborate <borate.tushar@gmail.com>
2023-10-28 14:27:55 +0530

    Cuisine Array Added Using Join

--- App.js
+++ App.js
@@ -1834,12 +1834,18 @@ const Heading = () => (
 
 const RestoComponent = (props) => {
     const { image, name, locality, cuisine } = props.data;
+
+    const cuis = [];
+    cuisine.map((resto)=> {
+        cuis.push(resto.name);
+    })
+    // console.log(cuis);
     return (
         <div className='resto-card'>
             <img className='resto-img' src={image.url} alt='Pizza Hut'/>
             <div className='resto-card-content'>
                 <h3 className='resto-title'>{name}</h3>
-                <p className='resto-options'>{cuisine[0].name}</p>
+                <p className='resto-options'>{cuis.join(", ")}</p>
                 <p className='resto-location'>{locality.name}</p>
             </div>
         </div>
